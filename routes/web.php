2192433a780596bb472e466783c3662f6b94293b
neshafwana30 <5026231013@student.its.ac.id>
2025-05-30 14:29:47 +0700

    CRUD Lengkap

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 //jangan lupa import controller gaisssss
 use App\Http\Controllers\Link;
-use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\Pegawai2Controller;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiController;
+
 
 
 /*
@@ -56,21 +58,34 @@ Route::get('linktree', function () {
 });
 
 Route::get('ets', function () {
-    return view('index');
+    return view('index2');
 });
 
 Route::get('blog', [Link::class, 'helloworld']);
 
 Route::get('dosen', [Link::class, 'index']);
 
-Route::get('pegawai/{nama}', [PegawaiController::class, 'index']);
-Route::get('/formulir', [PegawaiController::class, 'formulir'] );
-Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+// Route::get('pegawai/{nama}', [Pegawai2Controller::class, 'index']);
+Route::get('/formulir', [Pegawai2Controller::class, 'formulir'] );
+Route::post('/formulir/proses', [Pegawai2Controller::class, 'proses']);
 
 //route blog
 Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+//CRUD Pegawai bes
+//ini versi read
+//route CRUD
+Route::get('/pegawai', [PegawaiController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiController::class, 'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiController::class, 'edit']);
+Route::post('/pegawai/update', [PegawaiController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiController::class, 'hapus']);
+
+
+
 
 
 
